68cc4d1ecd033609bb0a7c640bc936b3cc3664d7
AlyshaJohnson <alysha.johnson@live.co.uk>
2022-05-06 07:15:04 +0000

    remove results page and added content to index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,166 @@
+// questions
+let questions = [{
+    question: 'Question 1',
+    answers: [{
+        text: 'Answer A',
+        score: 2
+      },
+      {
+        text: 'Answer B',
+        score: 1
+      },
+      {
+        text: 'Answer C',
+        score: 3
+      },
+      {
+        text: 'Answer D',
+        score: 4
+      },
+    ]
+  },
+  {
+    question: 'Question 2',
+    answers: [{
+        text: 'Answer A',
+        score: 2
+      },
+      {
+        text: 'Answer B',
+        score: 1
+      },
+      {
+        text: 'Answer C',
+        score: 3
+      },
+      {
+        text: 'Answer D',
+        score: 4
+      },
+    ]
+  },
+  {
+    question: 'Question 3',
+    answers: [{
+        text: 'Answer A',
+        score: 2
+      },
+      {
+        text: 'Answer B',
+        score: 1
+      },
+      {
+        text: 'Answer C',
+        score: 3
+      },
+      {
+        text: 'Answer D',
+        score: 4
+      },
+    ]
+  },
+  {
+    question: 'Question 4',
+    answers: [{
+        text: 'Answer A',
+        score: 2
+      },
+      {
+        text: 'Answer B',
+        score: 1
+      },
+      {
+        text: 'Answer C',
+        score: 3
+      },
+      {
+        text: 'Answer D',
+        score: 4
+      },
+    ]
+  },
+  {
+    question: 'Question 5',
+    answers: [{
+        text: 'Answer A',
+        score: 2
+      },
+      {
+        text: 'Answer B',
+        score: 1
+      },
+      {
+        text: 'Answer C',
+        score: 3
+      },
+      {
+        text: 'Answer D',
+        score: 4
+      },
+    ]
+  },
+  {
+    question: 'Question 6',
+    answers: [{
+        text: 'Answer A',
+        score: 2
+      },
+      {
+        text: 'Answer B',
+        score: 1
+      },
+      {
+        text: 'Answer C',
+        score: 3
+      },
+      {
+        text: 'Answer D',
+        score: 4
+      },
+    ]
+  },
+  {
+    question: 'Question 7',
+    answers: [{
+        text: 'Answer A',
+        score: 2
+      },
+      {
+        text: 'Answer B',
+        score: 1
+      },
+      {
+        text: 'Answer C',
+        score: 3
+      },
+      {
+        text: 'Answer D',
+        score: 4
+      },
+    ]
+  },
+  {
+    question: 'Question 8',
+    answers: [{
+        text: 'Answer A',
+        score: 2
+      },
+      {
+        text: 'Answer B',
+        score: 1
+      },
+      {
+        text: 'Answer C',
+        score: 3
+      },
+      {
+        text: 'Answer D',
+        score: 4
+      },
+    ]
+  }
+]
+
 // get elements
 let nextButton = document.getElementById('btn-forward');
 let backButton = document.getElementById('btn-back');
@@ -8,129 +171,80 @@ let answerElement = document.getElementById('answer-form');
 let closeButton = document.getElementById('close-btn');
 let popUp = document.getElementById('pop-up');
 let overlay = document.getElementById('overlay');
+let counterContainer = document.getElementById('question-counter')
+let noOfQuestions = questions.length
 
-let shuffledQuestions, currentQuestionIndex, score;
-
-// button actions
-nextButton.addEventListener('click', setNextQuestion);
-backButton.addEventListener('click', previousQuestion);
-//submitButton.addEventListener('click', )
-helpButton.addEventListener('click', selectHelpButton);
-closeButton.addEventListener('click', closeHelp);
-
-// questions
-let questions = [
-  {
-    question: 'Question 1',
-    answers: [
-      { text: 'Answer A', score: 2},
-      { text: 'Answer B', score: 1},
-      { text: 'Answer C', score: 3},
-      { text: 'Answer D', score: 4},
-    ]
-  },
-  {
-      question: 'Question 2',
-      answers: [
-        { text: 'Answer A', score: 2},
-        { text: 'Answer B', score: 1},
-        { text: 'Answer C', score: 3},
-        { text: 'Answer D', score: 4},
-      ]
-  },
-  {
-      question: 'Question 3',
-      answers: [
-        { text: 'Answer A', score: 2},
-        { text: 'Answer B', score: 1},
-        { text: 'Answer C', score: 3},
-        { text: 'Answer D', score: 4},
-      ]
-  },
-  {
-      question: 'Question 4',
-      answers: [
-        { text: 'Answer A', score: 2},
-        { text: 'Answer B', score: 1},
-        { text: 'Answer C', score: 3},
-        { text: 'Answer D', score: 4},
-      ]
-  },
-  {
-      question: 'Question 5',
-      answers: [
-        { text: 'Answer A', score: 2},
-        { text: 'Answer B', score: 1},
-        { text: 'Answer C', score: 3},
-        { text: 'Answer D', score: 4},
-      ]
-  },
-  {
-      question: 'Question 6',
-      answers: [
-        { text: 'Answer A', score: 2},
-        { text: 'Answer B', score: 1},
-        { text: 'Answer C', score: 3},
-        { text: 'Answer D', score: 4},
-      ]
-  },
-  {
-      question: 'Question 7',
-      answers: [
-        { text: 'Answer A', score: 2},
-        { text: 'Answer B', score: 1},
-        { text: 'Answer C', score: 3},
-        { text: 'Answer D', score: 4},
-      ]
-  },
-  {
-      question: 'Question 8',
-      answers: [
-        { text: 'Answer A', score: 2},
-        { text: 'Answer B', score: 1},
-        { text: 'Answer C', score: 3},
-        { text: 'Answer D', score: 4},
-      ]
-  }
-]
+let shuffledQuestions, currentQuestionIndex, score, counter;
 
 // start quiz on load
-window.addEventListener('load',loadQuiz);
-//window.addEventListener('load', selectHelpButton);
+window.addEventListener('load', loadQuiz);
 
 function loadQuiz() {
-    console.log('loadQuiz');
-    setNextQuestion();
-    score = 0;
+  console.log('loadQuiz');
+  initEventListeners();
+  startQuiz();
 }
 
-// set next question
-function setNextQuestion() {
-    console.log('setNextQuestion');
-    shuffledQuestions = questions.sort(() => Math.random() - 0.5);
-    currentQuestionIndex = 0;
-    showQuestion(shuffledQuestions[currentQuestionIndex]);
+function initEventListeners() {
+  // button actions
+  nextButton.addEventListener('click', nextQuestion);
+  backButton.addEventListener('click', previousQuestion);
+  submitButton.addEventListener('click', submitQuiz)
+  helpButton.addEventListener('click', selectHelpButton);
+  closeButton.addEventListener('click', closeHelp);
+
+  //add event listeners to answer buttons
+  for (let i = 0; i < 4; i++) {
+    document.getElementById("answer-"+(i + 1)).addEventListener('click', selectAnswer);
+  }
 }
 
-// show question
-function showQuestion (question) {
-  console.log('showQuestion');
+// start quiz
+function startQuiz() {
+  console.log('startQuiz');
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  counter = 0;
+  score = 0;
+  nextQuestion();
+}
+
+// next question
+function nextQuestion() {
+  console.log('nextQuestion');
+  showNextQuestion(shuffledQuestions[currentQuestionIndex]);
+  currentQuestionIndex++;
+}
+
+// show next question
+function showNextQuestion(question) {
+  console.log('showNextQuestion');
   questionElement.innerText = question.question;
-  question.answers.forEach(answer => {
-    let button = document.createElement('button');
-    button.innerText = answer.text;
-    button.classList.add('btn');
+  question.answers.forEach((answer, i) => {
+    document.getElementById('answer-'+(i+1)).innerText = answer.text;
   });
+  //if question is last then show submit button
+  if (counter === noOfQuestions) {
+    submitButton.classList.remove('hide');
+    nextButton.classList.add('hide');
+  };
+  // progress counter
+  counter++;
+  counterContainer.innerText = `${counter}/${noOfQuestions}`;
 }
 
 //select answer
-function selectAnswer () {
-  
+function selectAnswer() {
+
 }
+
+// block user from submitting no answer
+
 
 // submit quiz
 function submitQuiz() {
-    console.log('submitQuiz');
+  console.log('submitQuiz');
+
 }
 
 // count score - need to check love maths example
@@ -139,14 +253,14 @@ function submitQuiz() {
 
 // load previous question - optional at this point
 function previousQuestion() {
-    console.log('previousQuestion');
+  console.log('previousQuestion');
 }
 
 // select help button
 function selectHelpButton() {
-    console.log('selectHelpButton');
-    popUp.classList.add('active');
-    overlay.classList.add('active');
+  console.log('selectHelpButton');
+  popUp.classList.add('active');
+  overlay.classList.add('active');
 }
 
 // close help
